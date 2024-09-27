6fa6aa2c19b023dbd2af14b7c3047e8df6fe6533
shivanibhosale2003 <bhosaleshivani337@gmail.com>
2024-09-27 17:30:03 +0530

    learn loops in javascript

--- Day20/a.js
+++ Day20/a.js
@@ -1,7 +1,30 @@
-let str=prompt("enter a string");
-if(str.length==0){
-    console.log("string is empty");
+for(let i=1;i<=15;i=i+2){
+    console.log(i);
 }
-else{
-    console.log("string is not empty");
+
+for(let j=2;j<=10;j=j+2){
+    console.log(j);
+}
+
+//backward
+for(let j=10;j>=2;j=j-2){
+    console.log(j);
+}
+
+for(let k=5;k<=50;k=k+5){
+    console.log(k);
+}
+
+let n=prompt("write a number:");
+n=parseInt(n);
+
+for(let i=n;i<=n*10;i=i+n){
+    console.log(i);
+}
+
+
+for(let c=1;c<=4;c++){
+    for(let d=1;d<=3;d++){
+        console.log(d);
+    }
 }
